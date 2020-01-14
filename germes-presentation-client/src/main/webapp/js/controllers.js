bfe2ae35bf150236e8d240607c6a8b66ba138e23
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-01-14 17:42:34 +0200

    web-client: tests for controller functions

--- germes-presentation-client/src/main/webapp/js/controllers.js
+++ germes-presentation-client/src/main/webapp/js/controllers.js
@@ -1,5 +1,11 @@
 app.controller('CityCtrl', ['$scope', 'cityService', function($scope, cityService) {
         $scope.rowsPerPage = 10;
         $scope.cities = cityService.query();
+        $scope.isRegionCenter = function(city) {
+            if(city.district) {
+                return false;
+            }
+            return true;
+        };
     }
 ]);
