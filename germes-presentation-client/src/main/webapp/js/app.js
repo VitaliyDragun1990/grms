652cfcae1601f8ce8cf459c853f1317af09d9498
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-01-14 15:47:06 +0200

    rename client module

---
+++ germes-presentation-client/src/main/webapp/js/app.js
@@ -0,0 +1,7 @@
+'use strict'
+var app = angular.module('app', ['ngResource']);
+
+app.factory('cityService', ['$resource', function($resource) {
+        return $resource('/api/cities');
+}
+]);
---
+++ germes-presentation-client/src/main/webapp/js/controllers.js
@@ -0,0 +1,4 @@
+app.controller('CityCtrl', ['$scope', 'cityService', function($scope, cityService) {
+        $scope.cities = cityService.query();
+    }
+]);
