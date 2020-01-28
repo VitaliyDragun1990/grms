891190e7e0afee7f5b2f66d791b828bdac24fa0d
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-01-28 19:21:52 +0200

    unit tests for angular module

--- germes-presentation-web/client/src/app/app.component.spec.ts
+++ germes-presentation-web/client/src/app/app.component.spec.ts
@@ -1,27 +1,38 @@
+import { CityService } from './city.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageComponent } from './language/language.component';
+import { CitiesComponent } from './cities/cities.component';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 describe('AppComponent', () => {
+
+  let httpController: HttpTestingController;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, CitiesComponent, LanguageComponent
       ],
+      imports: [TranslateModule.forRoot(), HttpClientTestingModule],
+      providers: [TranslateService, CityService]
     }).compileComponents();
+
+    httpController = TestBed.get(HttpTestingController);
   }));
+
+  afterEach(() => {
+    httpController.verify();
+  });
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
+    let cityRequest =httpController.expectOne('api/cities');
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+    cityRequest.flush({});
   }));
 });
--- germes-presentation-web/client/src/app/city.service.spec.ts
+++ germes-presentation-web/client/src/app/city.service.spec.ts
@@ -1,15 +1,42 @@
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { CityService } from './city.service';
 
 describe('CityService', () => {
+
+  let httpController: HttpTestingController;
+  let cityService: CityService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [CityService]
     });
+
+    httpController = TestBed.get(HttpTestingController);
+    cityService = TestBed.get(CityService);
   });
 
-  it('should be created', inject([CityService], (service: CityService) => {
-    expect(service).toBeTruthy();
-  }));
+  afterEach(() => {
+    httpController.verify();
+  });
+
+  it('should be created', () => {
+    expect(cityService).toBeTruthy();
+  });
+
+  it('service should return two cities', () => {
+    const dummyResponse = [{ name: 'Odessa', region: 'Odessa', district: '' },
+    { name: 'Kyiv', region: 'Kyiv', district: '' }];
+
+    cityService.getCities().subscribe(cities => {
+      expect(cities.length).toBe(2);
+      expect(cities).toEqual(dummyResponse);
+    });
+
+    let cityRequest = httpController.expectOne('api/cities');
+    cityRequest.flush(dummyResponse);
+  });
 });
