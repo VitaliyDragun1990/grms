0d268acb4304f24942c1248659764cb1f817192a
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-02-17 18:56:02 +0200

    add unit tests to germes-common module

---
+++ germes-common/src/test/java/com/revenat/germes/infrastructure/transform/impl/helper/cached/CachedFieldProviderTest.java
@@ -0,0 +1,93 @@
+package com.revenat.germes.infrastructure.transform.impl.helper.cached;
+
+import com.revenat.germes.infrastructure.transform.annotation.DomainProperty;
+import com.revenat.germes.infrastructure.transform.impl.helper.BaseFieldProvider;
+import com.revenat.germes.infrastructure.transform.impl.helper.FieldManager;
+import com.revenat.germes.infrastructure.transform.impl.helper.FieldProvider;
+import com.revenat.germes.infrastructure.transform.impl.helper.SimilarFieldsLocator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Vitaliy Dragun
+ */
+@DisplayName("cached field provider")
+@ExtendWith(MockitoExtension.class)
+class CachedFieldProviderTest {
+
+    private FieldProvider actualFieldProvider;
+
+    @BeforeEach
+    void setUp() {
+        actualFieldProvider = new BaseFieldProvider(new SimilarFieldsLocator(), new FieldManager());
+    }
+
+    @Test
+    void shouldProvideSimilarFieldNames() {
+        final CachedFieldProvider fieldProvider = new CachedFieldProvider(actualFieldProvider);
+
+        final List<String> result = fieldProvider.getSimilarFieldNames(Source.class, Destination.class);
+
+        assertThat(result, hasSize(1));
+        assertThat(result, hasItem(equalTo("value")));
+    }
+
+    @Test
+    void shouldCacheSimilarFieldNamesForSameClassPairs(@Mock final FieldProvider fieldProviderMock) {
+        final CachedFieldProvider fieldProvider = new CachedFieldProvider(fieldProviderMock);
+        when(fieldProviderMock.getSimilarFieldNames(Source.class, Destination.class)).thenReturn(List.of("value"));
+
+        fieldProvider.getSimilarFieldNames(Source.class, Destination.class);
+        fieldProvider.getSimilarFieldNames(Source.class, Destination.class);
+        fieldProvider.getSimilarFieldNames(Source.class, Destination.class);
+
+        verify(fieldProviderMock, times(1)).getSimilarFieldNames(Source.class, Destination.class);
+    }
+
+    @Test
+    void shouldProvideDomainPropertyFields() {
+        final CachedFieldProvider fieldProvider = new CachedFieldProvider(actualFieldProvider);
+
+        final List<String> result = fieldProvider.getDomainPropertyFields(Destination.class);
+
+        assertThat(result, hasSize(1));
+        assertThat(result, hasItem(equalTo("text")));
+    }
+
+    @Test
+    void shouldCacheDomainPropertyFieldsForSameClass(@Mock final FieldProvider fieldProviderMock) {
+        final CachedFieldProvider fieldProvider = new CachedFieldProvider(fieldProviderMock);
+        when(fieldProviderMock.getDomainPropertyFields(Destination.class)).thenReturn(List.of("text"));
+
+        fieldProvider.getDomainPropertyFields(Destination.class);
+        fieldProvider.getDomainPropertyFields(Destination.class);
+        fieldProvider.getDomainPropertyFields(Destination.class);
+
+        verify(fieldProviderMock, times(1))
+                .getDomainPropertyFields(ArgumentMatchers.any(Class.class));
+    }
+
+    static class Source {
+        int value;
+    }
+
+    static class Destination {
+        int value;
+
+        @DomainProperty("test")
+        String text;
+    }
+}
