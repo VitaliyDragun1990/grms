0e90103043e0efb8357b8aa9ca9be931fa52cbdc
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-02-22 14:03:56 +0200

    user-service: simplify UserAppInitializer

--- germes-user-service/src/main/java/com/revenat/germes/user/infrastructure/web/initializer/UserAppInitializer.java
+++ germes-user-service/src/main/java/com/revenat/germes/user/infrastructure/web/initializer/UserAppInitializer.java
@@ -1,32 +1,27 @@
 package com.revenat.germes.user.infrastructure.web.initializer;
 
 import com.revenat.germes.user.infrastructure.config.UserSpringConfig;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
  * Loads Spring configuration for User service application on web container startup
  *
  * @author Vitaliy Dragun
  */
-public class UserAppInitializer implements WebApplicationInitializer {
+public class UserAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
-    public void onStartup(final ServletContext servletContext) throws ServletException {
-        final AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(UserSpringConfig.class);
+    protected String[] getServletMappings() {
+        return new String[] {"/*"};
+    }
 
-        servletContext.addListener(new ContextLoaderListener(context));
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
 
-        final DispatcherServlet servlet = new DispatcherServlet(context);
-        final ServletRegistration.Dynamic registration = servletContext.addServlet("api", servlet);
-        registration.setLoadOnStartup(1);
-        registration.addMapping("/*");
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { UserSpringConfig.class };
     }
 }
