79be2c6a0ad6f956046bbf4d174ad155ecd194a8
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-02-27 17:54:49 +0200

    gateway-service: add integration test for GatewayHandlerMapping

--- germes-gateway/src/test/java/com/revenat/germes/gateway/infrastructure/config/GatewayHandlerMappingTest.java
+++ germes-gateway/src/test/java/com/revenat/germes/gateway/infrastructure/config/GatewayHandlerMappingTest.java
@@ -1,4 +1,4 @@
-package com.revenat.germes.gateway.presentation.security.interceptor;
+package com.revenat.germes.gateway.infrastructure.config;
 
 import com.revenat.germes.gateway.GatewayApplication;
 import com.revenat.germes.gateway.domain.model.token.TokenProcessor;
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,8 +22,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringJUnitWebConfig(GatewayApplication.class)
 @AutoConfigureMockMvc
 @TestPropertySource("classpath:application.properties")
-@DisplayName("JWT interceptor")
-class JwtInterceptorTest {
+@DisplayName("Gateway handler mapping")
+class GatewayHandlerMappingTest {
 
     private static final String USER_NAME = "test";
     @Autowired
@@ -32,33 +33,14 @@ class JwtInterceptorTest {
     private TokenProcessor tokenProcessor;
 
     @Test
-    void shouldReturnStatusUnauthorizedIfRequestMissesAuthorizationHeader() throws Exception {
-        final ResultActions result = mockMvc.perform(get("/trip"));
-
-        result
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    void shouldReturnStatusUnauthorizedIfRequestContainsInvalidAuthorizationToken() throws Exception {
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth("invalid-token");
-
-        final ResultActions result = mockMvc.perform(get("/trip").headers(headers));
-
-        result
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    void shouldReturnStatusOkIfRequestContainsCorrectAuthorizationToken() throws Exception {
+    void shouldReturnStatusNotFoundIfMappedRouteIsNotFound() throws Exception {
         final String authToken = tokenProcessor.generateToken(USER_NAME);
         final HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(authToken);
 
-        final ResultActions result = mockMvc.perform(get("/trip").headers(headers));
+        final ResultActions result = mockMvc.perform(get("/aaaa").headers(headers));
 
         result
-                .andExpect(status().isOk());
+                .andExpect(status().isNotFound());
     }
 }
