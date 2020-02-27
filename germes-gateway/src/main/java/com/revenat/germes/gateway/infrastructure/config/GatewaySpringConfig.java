0667d7fcbee9344d8893fafe621a7393ef226eb1
Vitaliy Dragun <vdrag00n90@gmail.com>
2020-02-27 15:06:13 +0200

    add JwtInterceptor to check if request is authorized

--- germes-gateway/src/main/java/com/revenat/germes/gateway/infrastructure/config/GatewaySpringConfig.java
+++ germes-gateway/src/main/java/com/revenat/germes/gateway/infrastructure/config/GatewaySpringConfig.java
@@ -1,10 +1,38 @@
 package com.revenat.germes.gateway.infrastructure.config;
 
+import com.revenat.germes.gateway.application.security.token.TokenProcessor;
+import com.revenat.germes.gateway.presentation.security.interceptor.JwtInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
+ * Java-based configuration for Gateway application
+ *
  * @author Vitaliy Dragun
  */
 @Configuration
 public class GatewaySpringConfig {
+
+    @Configuration
+    public static class SecurityConfig implements WebMvcConfigurer {
+
+        @Autowired
+        private List<HandlerInterceptor> interceptors;
+
+        @Override
+        public void addInterceptors(final InterceptorRegistry registry) {
+            interceptors.forEach(registry::addInterceptor);
+        }
+
+        @Bean
+        public HandlerInterceptor jwtInterceptor(final TokenProcessor tokenProcessor) {
+            return new JwtInterceptor(tokenProcessor);
+        }
+    }
 }
---
+++ germes-gateway/src/main/java/com/revenat/germes/gateway/presentation/security/interceptor/JwtInterceptor.java
@@ -0,0 +1,54 @@
+package com.revenat.germes.gateway.presentation.security.interceptor;
+
+import com.revenat.germes.gateway.application.security.token.TokenProcessor;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * @author Vitaliy Dragun
+ */
+@RequiredArgsConstructor
+public class JwtInterceptor implements HandlerInterceptor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtInterceptor.class);
+
+    private final TokenProcessor tokenProcessor;
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
+            throws Exception {
+        final String authToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        final Optional<String> userName = authorize(authToken);
+
+        return userName.isPresent();
+    }
+
+    private Optional<String> authorize(final String authToken) {
+        try {
+            final String userName = tokenProcessor.getUserName(authToken);
+            return Optional.of(userName);
+        } catch (final Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            return Optional.empty();
+        }
+    }
+
+    private void requireAuthorization(final HttpServletResponse response) {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        try {
+            response.getOutputStream().flush();
+        } catch (final IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+}
